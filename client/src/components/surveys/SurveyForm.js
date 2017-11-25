bb7151c9cedfc5a6c51819c102bfb8046b5ec7a3
Eudemonyu <yulihou@gmail.com>
2017-11-24 23:17:30 -0600

    Add survey form and senfgrid

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,60 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+import formFields from './formFields';
+import validateEmails from '../../utils/validateEmails';
+
+class SurveyForm extends Component {
+  renderFields() {
+    return _.map(formFields, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            <i className="material-icons left">navigate_before</i>
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">navigate_next</i>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.recipients || '');
+
+  _.each(formFields, ({ name, noValueError }) => {
+    if (!values[name]) {
+      errors[name] = noValueError;
+    }
+  });
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'surveyForm',
+  destroyOnUnmount: false
+})(SurveyForm);
---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,43 @@
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import formFields from './formFields';
+import * as actions from '../../actions';
+
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h5>Please confirm your survey!</h5>
+      {reviewFields}
+      <button className="red btn-flat white-text" onClick={onCancel}>
+        <i className="material-icons left">navigate_before</i>
+        Back
+      </button>
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues, history)}
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
+      </button>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    formValues: state.form.surveyForm.values
+  };
+}
+
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
